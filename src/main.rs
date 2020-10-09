7dfa76859c7462e1251e63090304e697c9b59b93
nebelgrau <nebelgrau@gmx.com>
2020-10-09 13:13:24 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use nrf52840_hal as hal;
+
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+use hal::gpio::Level;
+use hal::delay::Delay;
+
+use hal::spim::Spim;
+
+use cortex_m_rt::entry;
+
+use apa102_spi as apa102;
+use apa102::Apa102;
+use smart_leds_trait::RGB8;
+use smart_leds::SmartLedsWrite;
+
+#[entry]
+fn main() -> ! {
+    
+    let p = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let port1 = hal::gpio::p1::Parts::new(p.P1);
+    
+    let di = port0.p0_08.into_push_pull_output(Level::Low).degrade(); // data line
+    let ci = port1.p1_09.into_push_pull_output(Level::Low).degrade(); // clock
+    let nc = port1.p1_14.into_floating_input().degrade(); // not connected
+
+    let pins = hal::spim::Pins{
+                sck: ci, 
+                miso: Some(nc), 
+                mosi: Some(di)};
+
+    let spi = Spim::new(
+        p.SPIM2,
+        pins,
+        hal::spim::Frequency::K500,
+        hal::spim::MODE_0,
+        0,
+    );
+
+    let mut delay = Delay::new(core.SYST);
+
+    let mut dotstar = Apa102::new(spi);
+    
+    let state0: [RGB8; 1] = [RGB8 { r: 16, g: 0, b: 16 }]; //purple
+    let state1: [RGB8; 1] = [RGB8 { r: 16, g: 16, b: 0 }]; //yellow
+    let state2: [RGB8; 1] = [RGB8 { r: 0, g: 16, b: 16 }]; //aqua
+    
+    loop {
+        dotstar.write(state0.iter().cloned()).unwrap();
+        delay.delay_ms(1000 as u16);
+        dotstar.write(state1.iter().cloned()).unwrap();
+        delay.delay_ms(1000 as u16);
+        dotstar.write(state2.iter().cloned()).unwrap();
+        delay.delay_ms(1000 as u16);
+    }
+    
+}
