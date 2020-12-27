f5c33b187df3dc7e2f7378f12eff039bc68bc2b4
nebelgrau <nebelgrau@gmx.com>
2020-12-27 18:40:20 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+### APA102 BLINK EXAMPLE WITH RTC-CONTROLLED TIMING
+
+Cycles through multiple hues with a predefined step. Each cycle lasts 1 second, timing controlled by RTC interrupts.
+
+![ItsyBitsy Neopixel](neopixel.gif)
+
+Ported to the Adafruit ItsyBitsy nRF52840 Express board (https://learn.adafruit.com/adafruit-itsybitsy-nrf52840-express) from these examples:
+* https://github.com/smart-leds-rs/smart-leds-samples/blob/master/trinket-m0-examples/examples/trinket_apa102_onboard_blink.rs
+* https://github.com/nrf-rs/nrf-hal/blob/master/examples/spi-demo/src/main.rs
+* https://github.com/nrf-rs/nrf-hal/tree/master/examples/rtc-demo
+
+### HOW TO FLASH:
+
+* build the code: ```cargo build --release```
+* convert to .hex file: ```arm-none-eabi-objcopy -O ihex target/thumbv7em-none-eabihf/release/blinky_neopixel neopixel.hex```
+* create a dfu package: ```adafruit-nrfutil dfu genpkg --dev-type 0x0052 --application neopixel.hex neopixel.zip```
+* put the board into bootloader mode (double click on reset button, will show up as _ITSY840BOOT_ or similar)
+* flash the firmware: ```adafruit-nrfutil dfu serial --package neopixel.zip -p /dev/ttyACM0 -b 115200```
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use nrf52840_hal as hal;
+
+use hal::{pac::{
+            //CorePeripherals, 
+            Peripherals},
+        prelude::*,
+        gpio::Level,
+        //delay::Delay,
+        spim::Spim,
+        rtc::{Rtc, RtcCompareReg, RtcInterrupt},        
+        };
+
+use cortex_m_rt::entry;
+
+use apa102_spi::Apa102;
+
+use smart_leds_trait::RGB8;
+use smart_leds::{
+    hsv::{hsv2rgb, Hsv},
+    SmartLedsWrite,
+    };
+
+const PXL_SATURATION: u8 = 255;
+const PXL_VALUE: u8 = 12; // how bright the NeoPixel is
+const PXL_HUE_STEP: u8 = 32; //the smaller the step, the more hues
+
+#[entry]
+fn main() -> ! {
+    
+    let p = Peripherals::take().unwrap();
+    //let core = CorePeripherals::take().unwrap();
+
+    let clocks = hal::clocks::Clocks::new(p.CLOCK);
+    let _clocks = clocks.start_lfclk();
+
+    // set up Real Time Clock, interrupt will fire after one second
+    let mut rtc = Rtc::new(p.RTC0,0).unwrap();
+    rtc.set_compare(RtcCompareReg::Compare0, 32_768).unwrap();
+    rtc.enable_event(RtcInterrupt::Compare0);
+
+    // set up GPIO ports
+    let port0 = hal::gpio::p0::Parts::new(p.P0);
+    let port1 = hal::gpio::p1::Parts::new(p.P1);
+    
+    // set up pins for the NeoPixel 
+    let di = port0.p0_08.into_push_pull_output(Level::Low).degrade(); // data line
+    let ci = port1.p1_09.into_push_pull_output(Level::Low).degrade(); // clock
+    let nc = port1.p1_14.into_floating_input().degrade(); // not connected
+
+    // use these pins for the SPI bus
+    let pins = hal::spim::Pins{
+                sck: ci, 
+                miso: Some(nc), 
+                mosi: Some(di)};
+
+    // set up SPI                
+    let spi = Spim::new(
+        p.SPIM2,
+        pins,
+        hal::spim::Frequency::K500,
+        hal::spim::MODE_0,
+        0,
+    );
+
+    
+    // set up the NeoPixel
+    let mut dotstar = Apa102::new(spi);
+        
+    rtc.enable_counter(); // start the RTC
+
+    let mut pxl_hue: u8 = 0; //starting hue
+
+    loop {
+
+        while !rtc.is_event_triggered(RtcInterrupt::Compare0) {}
+        rtc.reset_event(RtcInterrupt::Compare0); // reset interrupt
+        rtc.clear_counter(); // zero the RTC counter again
+
+        let color: [RGB8; 1] = [hsv2rgb(Hsv {
+            hue: pxl_hue,
+            sat: PXL_SATURATION,
+            val: PXL_VALUE,
+            })];
+
+        dotstar.write(color.iter().cloned()).unwrap();
+        
+        pxl_hue = pxl_hue.wrapping_add(PXL_HUE_STEP);
+
+    }
+    
+}
